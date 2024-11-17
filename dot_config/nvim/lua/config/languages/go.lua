b1ca7ae75da384851a542ee35ba064a5c53a2adc
Gabriel Ribeiro <gabriel@logicsphere.co>
2024-11-17 10:53:05 +0000

    some updates

--- dot_config/nvim/lua/config/languages/go.lua
+++ dot_config/nvim/lua/config/languages/go.lua
@@ -8,3 +8,23 @@ vim.keymap.set("n", "<leader>ee", "<cmd>GoIfErr<cr>", {
 	noremap = true,
 	desc = "Manage errors in the current file",
 })
+
+-- debugger
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+
+dap.listeners.before.event_terminated.dapui_config = function()
+	dapui.close()
+end
+
+dap.listeners.before.event_exited.dapui_config = function()
+	dapui.close()
+end
+
+require("nvim-dap-virtual-text").setup()
--- dot_config/nvim/lua/plugins/go.lua
+++ dot_config/nvim/lua/plugins/go.lua
@@ -1,16 +1,26 @@
 return {
-    {
-        'ray-x/go.nvim',
-        dependencies = {
-            'ray-x/guihua.lua',
-            'neovim/nvim-lspconfig',
-            'nvim-treesitter/nvim-treesitter',
-        },
-        config = function()
-            require('go').setup()
-        end,
-        event = { 'CmdlineEnter' },
-        ft = { 'go', 'gomod' },
-        build = ':lua require("go.install").update_all_sync()',
-    }
+	{
+		"ray-x/go.nvim",
+		dependencies = {
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()',
+	},
+
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+		},
+	},
+
+	{ "theHamsta/nvim-dap-virtual-text" },
 }
